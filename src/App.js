5d94a5ed81671f732eb56a2adc3a1a3df8181544
junnity <69070737+junnity@users.noreply.github.com>
2023-08-18 18:08:40 +0900

    23.08.18 junnity

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import productData from './productData';
 import ShoesList from './ShoesList';
+import DetailPage from './detail';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState } from 'react';
 
+import {Routes, Route, Link} from 'react-router-dom'
 
 
 function App() {
 
   let [shoes,setShoes] = useState(productData)
   let [shoePic,setShoePic] = useState(['https://codingapple1.github.io/shop/shoes1.jpg','https://codingapple1.github.io/shop/shoes2.jpg','https://codingapple1.github.io/shop/shoes3.jpg'])
-
+  let [detailLink,setDetailLink] = useState(['/detail','/detail2','/detail3'])
 
   return (
     <div className="App">
 
-      {/* 내브바 */}
-      <Navbar bg="light" data-bs-theme="light">
+            {/* 내브바 */}
+            <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">신발가게</Navbar.Brand>
+          <Navbar.Brand href="/">신발가게</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#features">Product</Nav.Link>
             <Nav.Link href="#pricing">Cart</Nav.Link>
           </Nav>
@@ -32,20 +34,30 @@ function App() {
       </Navbar>
 
       {/* 대표이미지 */}
-      <div className="main-bg"></div> 
+      <div className="main-bg"></div>
 
-      {/* 상품리스트 */}
-      <div className="container">
-        <div className="row">
-          {shoes.map(function(a,i){
-            return (
-              <ShoesList num = {i} shoes = {shoes} shoePic = {shoePic}></ShoesList>
-            )
-          })}
+      <Link to="/" style={{margin:"30px"}}>홈</Link>
+      <Link to="/detail"style={{margin:"30px"}}>상세페이지</Link>
+
+      <Routes>
+        <Route path='/detail' element={<div><DetailPage/></div>} />
+        <Route path="/" element={
+          <>
+            {/* 상품리스트 */}
+            <div className="container">
+            <div className="row">
+             {shoes.map(function(a,i){
+              return (
+                <ShoesList  num = {i} shoes = {shoes} shoePic = {shoePic} detailLink={detailLink}></ShoesList>
+              )
+            })}
 
           
         </div>
       </div>
+          </>
+        }/>
+      </Routes>
     
     </div>
   );
--- src/ShoesList.js
+++ src/ShoesList.js
@@ -1,6 +1,8 @@
+import { useHref } from "react-router-dom";
+
 function ShoesList(props){
     return(
-      <div className="col-md-4" >
+      <div className="col-md-4">
         <img src={props.shoePic[props.num]} width="80%" />
         <h4>{props.shoes[props.num].title}</h4>
         <p>{props.shoes[props.num].price}</p>
